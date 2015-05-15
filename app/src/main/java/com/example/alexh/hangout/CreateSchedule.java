77033b807521f1d21c1d4094a8891f33a6eceafa
alexanderhorstman <alexander.horstman@yahoo.com>
2015-05-15 10:35:52 -0700

    Update

--- app/src/main/java/com/example/alexh/hangout/CreateSchedule.java
+++ app/src/main/java/com/example/alexh/hangout/CreateSchedule.java
@@ -8,11 +8,13 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CreateSchedule extends FragmentActivity implements AdapterView.OnItemSelectedListener {
+public class CreateSchedule extends FragmentActivity {
 
     static final String[] validTimes = {"12:00 - 12:30", "12:30 - 1:00", "1:00 - 1:30",
             "1:30 - 2:00", "2:00 - 2:30","2:30 - 3:00", "3:00 - 3:30", "3:30 - 4:00",
@@ -62,100 +64,40 @@ public class CreateSchedule extends FragmentActivity implements AdapterView.OnIt
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        CreateScheduleFragment scheduleFragment = new CreateScheduleFragment();
-        fragmentTransaction.replace(android.R.id.content, scheduleFragment);
-        fragmentTransaction.commit();
-        */
         setContentView(R.layout.create_schedule);
         View tempView = findViewById(R.id.create_activity_fragment);
         tempView.setVisibility(View.GONE);
         initializeAll();
 
-        /*
-        listArray = new ListView[7];
-        listArray[0] = (ListView) findViewById(R.id.sun_list_view);
-        listArray[1] = (ListView) findViewById(R.id.mon_list_view);
-        listArray[2] = (ListView) findViewById(R.id.tue_list_view);
-        listArray[3] = (ListView) findViewById(R.id.wed_list_view);
-        listArray[4] = (ListView) findViewById(R.id.thu_list_view);
-        listArray[5] = (ListView) findViewById(R.id.fri_list_view);
-        listArray[6] = (ListView) findViewById(R.id.sat_list_view);
-        activityListArray = new ActivityList[7];
-        adapter = new ListAdapter[7];
-        for(int i = 0; i < listArray.length; i++) {
-            final int index = i;
-            activityListArray[i] = new ActivityList();
-            adapter[i] = new ListAdapter(this, activityListArray[i].toStringArray(), activityListArray[i]);
-            listArray[i].setAdapter(adapter[i]);
-            listArray[i].setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    //pull up the create activity fragment
-                    Toast.makeText(getBaseContext(), activityListArray[index].getActivity(position)
-                    .toString(), Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        */
-
-        /*
-        Spinner dayOfWeekSpinner = (Spinner)  findViewById(R.id.day_of_week_spinner_create_profile);
-        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this,
-                R.array.days_of_week, android.R.layout.simple_spinner_dropdown_item);
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dayOfWeekSpinner.setAdapter(spinnerAdapter);
-        */
     }
 
-    public void addTimeToSchedule(View view) {
-        TextView newText = new TextView(getBaseContext());
-        //TextView fromTextView = (TextView) findViewById(R.id.from_text_view_create_schedule);
-        //TextView toTextView = (TextView) findViewById(R.id.to_text_view_create_schedule);
-        /*
-        switch (dayOfWeek) {
-            case "Sunday":
-                newText = (TextView) findViewById(R.id.sunday_text_view_create_schedule);
-                break;
-            case "Monday":
-                newText = (TextView) findViewById(R.id.monday_text_view_create_schedule);
-                break;
-            case "Tuesday":
-                newText = (TextView) findViewById(R.id.tuesday_text_view_create_schedule);
-                break;
-            case "Wednesday":
-                newText = (TextView) findViewById(R.id.wednesday_text_view_create_schedule);
-                break;
-            case "Thursday":
-                newText = (TextView) findViewById(R.id.thursday_text_view_create_schedule);
-                break;
-            case "Friday":
-                newText = (TextView) findViewById(R.id.friday_text_view_create_schedule);
-                break;
-            case "Saturday":
-                newText = (TextView) findViewById(R.id.saturday_text_view_create_schedule);
-                break;
+    public void addActivityToSchedule(View view) {
+        View activityView = findViewById(R.id.create_activity_fragment);
+        activityView.setVisibility(View.GONE);
+        Button startButton = (Button) findViewById(R.id.start_time_button_create_activity_fragment);
+        Button stopButton = (Button) findViewById(R.id.stop_time_button_create_activity_fragment);
+        EditText descriptionEditText = (EditText) findViewById(R.id.description_edit_text_create_activity);
+        String startTime;
+        String stopTime;
+        ScheduleActivity newActivity;
+        startTime = startButton.getText().toString();
+        stopTime = stopButton.getText().toString();
+        if(startTime.contains("am")) {
+            //test this
+            newActivity = new ScheduleActivity(Integer.parseInt(startTime.substring(0,
+                    startTime.indexOf(":"))), Integer.parseInt(startTime.substring
+                    (startTime.indexOf(":") + 1, startTime.indexOf(" "))), Integer.parseInt
+                    (startTime.substring(0, stopTime.indexOf(":"))), Integer.parseInt
+                    (stopTime.substring(startTime.indexOf(":") + 1, stopTime.indexOf(" "))),
+                    descriptionEditText.getText().toString(), true);
         }
-        newText.append(startPickerFragment.getTime() + " - " + stopPickerFragment.getTime() + ";  ");
-        */
-        //fromTextView.setText("From");
-        //toTextView.setText("To");
+        else {
+
+        }
     }
 
     public void finishProfile(View view) {
-
-    }
-
-    //method not being called when item is selected
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        dayOfWeek = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), position, Toast.LENGTH_SHORT).show();
-    }
-
-    public void onNothingSelected(AdapterView<?> parent) {
-
+        //do stuff to create and save a profile
     }
 
     public void pickStartTime(View v) {
